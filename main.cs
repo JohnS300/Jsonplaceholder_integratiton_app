64ab90b8dbbca571099965c548424c3597e36a31
JohnS300 <johnsofokleous@gmail.com>
2024-10-22 23:36:32 +0300

    Refacturing code

--- main.cs
+++ main.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace CoreConsole{
-    class Program{
-        static void Main(string[] args){
-            using(var client = new HttpClient()){
-                var endpoint = new Uri ("https://jsonplaceholder.typicode.com/posts");
-                //one line command to get the result back in json format
-                //var results = client.GetAsync(endpoint).Result.Content.ReadAsStringAsync().Result;
-                //but spliting it make it clearer to understand
-                var results = client.GetAsync(endpoint).Result;
-                var json = results.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(json);
+    public class Get{
+        using(var client = new HttpClient()){
+            var endpoint = new Uri ("https://jsonplaceholder.typicode.com/posts");
+            var newPost = new Post(){
+                Title = "Test Post",
+                Body = "Hello World",
+                UserId = 44,
             };
-        }
+            var newPostJson=JsonConvert
+        };
+
 
     }
 }
