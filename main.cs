59799b779a4f98fac231e8dd4ba2784ff3dcf724
JohnS300 <johnsofokleous@gmail.com>
2024-10-22 23:55:34 +0300

    Installed packeges and made Post request work

--- main.cs
+++ main.cs
@@ -3,22 +3,25 @@ using System.Net.Http;
 using Newtonsoft.Json;
 
 namespace CoreConsole{
-    public class Get{
-        using(var client = new HttpClient()){
-            var endpoint = new Uri ("https://jsonplaceholder.typicode.com/posts");
-            var newPost = new Post(){
-                Title = "Test Post",
-                Body = "Hello World",
-                UserId = 44,
+    
+    class Program{
+        static void Main(string[] args){
+
+            using(var client = new HttpClient()){
+                var endpoint = new Uri ("https://jsonplaceholder.typicode.com/posts");
+                var newPost = new Post(){
+                    Title = "Test Post",
+                    Body = "Hello World",
+                    UserId = 44,
+                };
+                var newPostJson=JsonConvert.SerializeObject(newPost);
+                Console.WriteLine("newPost");
+                Console.WriteLine(newPost);
+                Console.WriteLine("newPostJson");
+                Console.WriteLine(newPostJson);
+
             };
-            var newPostJson=JsonConvert.SerializeObject(newPost);
-            Console.WriteLine("newPost");
-            Console.WriteLine(newPost);
-            Console.WriteLine("newPostJson");
-            Console.WriteLine(newPostJson);
-
-        };
-
+        }
 
     }
 }
