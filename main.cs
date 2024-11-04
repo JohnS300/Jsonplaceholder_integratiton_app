6edac8f570c0fb137a294f7bd383fcd822b1938c
John <John.Sofokleous@azyra.com>
2024-11-04 23:07:54 +0200

    Revert "Installed packeges and made Post request work"
    This reverts commit 59799b779a4f98fac231e8dd4ba2784ff3dcf724.

---
+++ main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CoreConsole{
+    public class Get{
+        using(var client = new HttpClient()){
+            var endpoint = new Uri ("https://jsonplaceholder.typicode.com/posts");
+            var newPost = new Post(){
+                Title = "Test Post",
+                Body = "Hello World",
+                UserId = 44,
+            };
+            var newPostJson=JsonConvert.SerializeObject(newPost);
+            Console.WriteLine("newPost");
+            Console.WriteLine(newPost);
+            Console.WriteLine("newPostJson");
+            Console.WriteLine(newPostJson);
+
+        };
+
+
+    }
+}
